8219b9ea6738fcbcf4518d0cd6838bdddea6a52c
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-11 21:46:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,7 @@ def now_serving (array)
   else
     next_in_line = array.first
     puts "Currently serving #{next_in_line}."
-    array.unshift!
+    array.unshift = array
     return array
   end
 end
