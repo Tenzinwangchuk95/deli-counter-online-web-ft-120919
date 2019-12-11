73df32d87c5043d1af27f5fd10243f05280c5dfb
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-11 21:19:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else 
-   line_order = Array.new array.each do |name|
+   line_order = Array.new
+   array.each do |name|
       counter = 1
-      
       line_order << "#{counter}. #{name}"
       counter += 1
     end
